ffabd99771f414c57807ed1a435f5ff8686e8692
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-10-29 09:57:16 -0600

    formatted buttons, returns true/false for answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,35 +2,66 @@ window.onload = function () {
 
     var time = 29
 
-    var qOne = {
-        question: "What's my name, bitch?",
+    var q1 = {
+        question: "What's my name?",
         answers: ['bill', 'jake', 'fred', 'rick'],
+        correct: ['rick']
     }
 
-    var qTwo = {
+    var q2 = {
         question: "How many fingers in my pocket?",
         answers: ['6', '4', 'none', 'dumb question'],
     }
-
+    
     //populates questions and answers into appropriate divs
     function question(q) {
+
         document.getElementById('qDiv').innerText = (q).question;
         for (i = 4; i > 0; i--) {
             document.getElementById('aDiv' + [i]).innerText = (q).answers[i - 1];
+            document.getElementById('aDiv' + [i]).style.visibility = "visible";
         }
+    }
+
+    //true and false 
+    function true1() {
+        console.log("true");
+    }
+
+    function false1() {
+        console.log("false");
     }
 
     //timer counts down from 30
     function timer() {
+        document.getElementById('time').style.visibility = "visible";
         function f() {
-            if (time > 0) {
+            if (time > -1) {
                 document.getElementById('time').innerHTML = (time);
             }
             time--;
         }
         setInterval(f, 1000);
     }
-timer();
-question(qTwo);
 
+    $("#start").click(function () {
+        i=q1
+        timer();
+        question(i);
+
+        $(".test").click(function () {
+            i=q1;
+        console.log(this);
+        console.log(this.innerHTML);
+        console.log(i)
+        console.log((i).correct);
+            if (this.innerHTML==((i).correct)) {
+                true1();
+            }
+            else {
+                false1();
+            }
+        });
+
+    });
 }
